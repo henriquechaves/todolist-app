9743a22220751958b6cef049bc84128d2ed99c7a
Henrique Chaves <hnrqchvs@gmail.com>
2017-08-14 11:17:56 -0300

    comments

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,6 +1,8 @@
 const port = 3003
 
+// Faz o parser da requisção
 const bodyParser = require('body-parser')
+// Servidor web que roda em cima do node
 const express = require('express')
 const server = express()
 const AllowCors = require('./cors')
