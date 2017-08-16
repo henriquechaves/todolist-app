dfb8ee0e3b24f18a9920008bcf8b1310305a5e91
Henrique Chaves <hnrqchvs@gmail.com>
2017-08-16 17:37:36 -0300

    update comments

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,7 @@
 // Mapeamento e conexão
 const mongoose = require('mongoose')
+
 // Api de Promisses para excluir mensagem de advertência
 mongoose.Promise = global.Promise
+
 module.exports = mongoose.connect('mongodb://localhost/todo')
